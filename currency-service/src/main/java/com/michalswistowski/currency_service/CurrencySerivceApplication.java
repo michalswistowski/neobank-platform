7c12dfd4b18c8d3dd87972557fff70036df1f608
michalswistowski <michalswistowski2002@gmail.com>
2026-01-24 21:55:06 +0100

    Initial services creation

---
+++ currency-service/src/main/java/com/michalswistowski/currency_service/CurrencySerivceApplication.java
@@ -0,0 +1,13 @@
+package com.michalswistowski.currency_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencySerivceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencySerivceApplication.class, args);
+	}
+
+}
---
+++ currency-service/src/test/java/com/michalswistowski/currency_service/CurrencySerivceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michalswistowski.currency_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencySerivceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
